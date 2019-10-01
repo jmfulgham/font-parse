dc1e1cde94cb12194d90b52ee372e4e914ee2a68
Mone't Fulgham <monetfulgham@gmail.com>
2019-09-30 20:20:53 -0400

    Creating initial server and set up files

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const fontScrapeService = require('./fontScrapeService');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/', (req, res) => {
+    let url = req.query.url;
+
+    if (url) {
+        axios({
+            method: 'get',
+            url
+        }).then(resp => {
+            let htmlRes = resp.data;
+            fontScrapeService(htmlRes);
+        }).catch(e => `uh oh, ${e}`);
+    } else {
+        res.send("Please input a URL to scrape")
+    }
+});
