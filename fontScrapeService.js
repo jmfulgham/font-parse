41630a9f4b5642df747c1114375a275c5b874833
Mone't Fulgham <monetfulgham@gmail.com>
2019-10-08 14:29:07 -0400

    Reformat service to class and add dataParse method

--- fontScrapeService.js
+++ fontScrapeService.js
@@ -1,36 +1,56 @@
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
 const axios = require('axios');
+let allFonts;
 
-async function fontScrapeService(htmlRes) {
-    const dom = new JSDOM(htmlRes);
-    const document = dom.window.document;
-    let links = document.getElementsByTagName('link');
-    let css = [];
+class FontScrapeService {
 
-    for (let i = 0; i < links.length; i++) {
-        if (links.item(i).type === "text/css") {
-            css.push(axios.get(links.item(i).href).then(res => res.data))
+    async allLinksParse(htmlRes) {
+        const dom = new JSDOM(htmlRes);
+        const document = dom.window.document;
+        this.styleSheetParse(document);
+        let links = document.getElementsByTagName('link');
+        let css = [];
+
+        for (let i = 0; i < links.length; i++) {
+            if (links.item(i).type === "text/css") {
+                css.push(axios.get(links.item(i).href).then(res => res.data))
+            }
         }
+
+        let finishedCSS = await Promise.all(css);
+        this.dataParse(finishedCSS.toString());
+        return allFonts;
     }
 
-    let finishedCSS = await Promise.all(css);
-    let matches = dataParse(finishedCSS.toString());
-    let noDupes = new Set(matches);
-    return Array.from(noDupes);
+    dataParse(data) {
+        let fonts = [];
+        let fontFam = data.match(/(font-family\:.*?\;)/gm);
+        for (let style in fontFam) {
+            if (fontFam[style].includes("font-family")) {
+                const fontFamilyValue = fontFam[style].split(':');
+                fonts.push(fontFamilyValue[1]);
+            }
+        }
+        let noDupes = new Set(fonts);
+        allFonts = Array.from(noDupes);
+        return allFonts;
+    }
 
-}
-
-function dataParse(data) {
-    let fonts = [];
-    let fontFam = data.match(/(font-family\:.*?\;)/gm);
-    for (let style in fontFam) {
-        if (fontFam[style].includes("font-family")) {
-            const fontFamilyValue = fontFam[style].split(':');
-            fonts.push(fontFamilyValue[1]);
+    styleSheetParse(document) {
+        let style = document.getElementsByTagName('style');
+        if (!style) {
+            return "No Style Elements Found"
+        } else {
+            for (let i = 0; i < style.length; i++) {
+                let styleString = style.item(i).textContent;
+                if (styleString.includes("font-family")) {
+                    this.dataParse(styleString);
+                }
+            }
         }
     }
-    return fonts
 }
 
-module.exports = fontScrapeService;
+
+module.exports = FontScrapeService;
