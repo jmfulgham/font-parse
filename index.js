62582d05db1654de9fa166550c386b459b7e73bf
Mone't Fulgham <monet_fulgham@homedepot.com>
2020-01-08 13:43:32 -0500

    Update query param to get URL

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const FontScrapeService = require("./fontScrapeService");
 const fontScrapeService = new FontScrapeService();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/parseFont', (req, res) => {
-    let url = req.body.url;
+app.post('/parseFonts', (req, res) => {
+    let url = req.query.url;
+    console.log(url);
     if (!url) {
         res.status(404).send("Please input a URL to scrape")
     } else {
